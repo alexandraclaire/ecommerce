93e902bfc34893cea9ef6247ce8c5f4506868626
Alexandra Carter <alexandracarter@Alexandras-MacBook-Air.local>
2015-11-06 11:36:08 +1300

    final styling 6 november

--- footer.php
+++ footer.php
@@ -15,11 +15,28 @@
                 
    
     <div id"contact">
+    	<div class="contact">
         <h2>Visit Our Store</h2>
         <p>1 Street Road<br>Townsville<br>City</p>
-    </div>
-     <p class="disclaimer">This is a demo site for educational purposes only. No orders shall be fulfilled.</p>                
+       </div> 
+        <div class="social">
+     	<?php if( get_theme_mod( 'active_social' ) == '') : ?> 
+                        
+                    		<?php if ( is_active_sidebar('social-widget-area') ) : ?>
+                        
+                            		<?php dynamic_sidebar('social-widget-area');  ?>
 
+                            
+                    		<?php endif; ?> 
+                            
+                    	<?php endif; ?>
+     </div>	
+    </div>
+       
+     <div class="disclaimer">
+     	<p>This is a demo site for educational purposes only. No orders shall be fulfilled.</p>
+     </div>	                
+  
 </footer>
 <?php wp_footer(); ?>
 
